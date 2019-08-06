8f07e2b72b3afee5c0d48aa396f26b85cc527ff3
Ranispoorti <43165149+Ranispoorti@users.noreply.github.com>
2019-08-06 15:57:53 +0530

    Add files via upload

---
+++ rani.py
@@ -0,0 +1,11 @@
+def check(n):
+  if n>0 :
+    print (n,"is a positive number")
+  elif n<0 :
+    print (n,"is a negative number")
+  else :
+    print (n,"is a zero number")
+
+
+check(5)
+check(-1)
